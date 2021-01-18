bbe1db0ccae6378bfad54260ed353230047974b7
michaelchilan <michael.chilan@epn.edu.ec>
2021-01-17 20:18:12 -0500

    Correction of the execution information updated and verified.

--- src/main/java/ec/edu/epn/git/asistente/AssistantExecute.java
+++ src/main/java/ec/edu/epn/git/asistente/AssistantExecute.java
@@ -4,11 +4,18 @@ public class AssistantExecute {
     public static void main(String[] args) {
         System.out.println("== Assistant Execute ==");
 
-        Assistant av = new Assistant();
-        boolean as1 = av.resultAccepted("Hola Asistente");
-        System.out.println("EL comando ha sido aceptado si es verdadero.\nPor lo tanto es: " + as1);
+        String command1 = "Hola Asistente";
+        String command2 = "Busca el documento";
 
-        boolean as2 = av.assistantControl("Busca el documento", "Text1.doc", "");
-        System.out.println("El resultado de la búsqueda es: " + as1);
+        String doc1 = "Text1.doc";
+
+        Assistant av = new Assistant();
+        boolean as1 = av.resultAccepted(command1);
+        if(as1)
+           System.out.println("EL comando '--" + command1 + "--' será aceptado si es verdadero.\nPor lo tanto es satisfactorio.");
+
+        boolean as2 = av.assistantControl(command2, doc1, "");
+        if(as2)
+            System.out.println("El comando '--" + command2 + "--' ha sido aceptado.\n" + "El resultado de la búsqueda del documento " + doc1 + " es Satisfactorio.");
     }
 }
