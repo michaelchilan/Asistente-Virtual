e90e274f9d0d9e611e2bfad5d38756a1c136cef2
michaelchilan <michael.chilan@epn.edu.ec>
2021-01-17 21:01:23 -0500

    Aggregation of execution examples with the resulting commands new executions

--- src/main/java/ec/edu/epn/git/asistente/AssistantExecute.java
+++ src/main/java/ec/edu/epn/git/asistente/AssistantExecute.java
@@ -17,5 +17,24 @@ public class AssistantExecute {
         boolean as2 = av.assistantControl(command2, doc1, "");
         if(as2)
             System.out.println("El comando '--" + command2 + "--' ha sido aceptado.\n" + "El resultado de la búsqueda del documento " + doc1 + " es Satisfactorio.");
+
+        System.out.println("\n\n== New Executions ==");
+
+        String command3 = "Lee el documento";
+        String command4 = "Escribe en el documento";
+        String command7 = "Salir Asistente";
+
+        String doc2 = "Text4.doc";
+        String doc3 = "Test2.doc";
+
+        String text = "ESTA ES UNA PRUEBA PARA LA EJECUCIÓN DEL COMANDO DE ESCRITURA";
+
+        boolean as3 = av.assistantControl(command3, doc2, "");
+        if(as3)
+            System.out.println("EL comando '--" + command3 + "--' será aceptado si es verdadero.\nEl contenido del documento es:\n" + doc2);
+
+        boolean as4 = av.assistantControl(command4, doc3, text);
+        if(as4)
+            System.out.println("EL comando '--" + command4 + "--' será aceptado si es verdadero.\nEl texto agregado es:\n" + text);
     }
 }
